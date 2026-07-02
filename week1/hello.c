a8c7da307dda5e1c461894b7a96bfe57bb7783a0
ศุภณัฐ ด้วงโน <tiwter081@gmail.com>
2026-07-02 16:21:35 +0700

    Add initial markdown files for week 2 exercises
    - Created ex01_flowchart.md for flowchart exercise
    - Created ex02_pseudocode.md for pseudocode exercise
    - Created ex03_game_algorithm.md for game algorithm exercise

---
+++ week1/hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+    printf("สวัสดีครับ! ผม ศุภณัฐ ด้วงโน\n");
+    printf("รหัสนักศึกษา: 692110170\n");
+    printf("วิชา:DG111 Computer Programming\n");
+    return 0;
+}
